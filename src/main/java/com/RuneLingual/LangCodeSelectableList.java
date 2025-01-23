a6d5ca854aebcbe0f5b5a71ed8b36eaf4c89f259
IaKee <giordano.s.mail@gmail.com>
2025-01-23 15:51:08 -0300

    re-included brazilian portuguese

--- src/main/java/com/RuneLingual/LangCodeSelectableList.java
+++ src/main/java/com/RuneLingual/LangCodeSelectableList.java
@@ -8,11 +8,55 @@ import javax.inject.Inject;
 @Getter
 public enum LangCodeSelectableList
 {
-    ENGLISH ("en", "EN","EN", 8, 6, false, false, false, false, true),
-    //PORTUGUÊS_BRASILEIRO ("pt_br", "PT","PT-BR", 8, 6, false, false, false, false, true),
-    //NORSK("no", "NB", "NB", 8, 6, false, false, false, false, true),
-    日本語("ja", "JA", "JA", 15, 13, true, true, true, true, false);
-    // todo: add languages here
+    ENGLISH (
+        "en",
+        "EN",
+        "EN",
+        8,
+        6,
+        false,
+        false,
+        false,
+        false,
+        true),
+    PORTUGUÊS_BRASILEIRO (
+        "pt_br",
+        "PT",
+        "PT-BR",
+        8,
+        6,
+        false,
+        false,
+        false,
+        false,
+        true),
+    日本語(
+        "ja",
+        "JA",
+        "JA",
+        15,
+        13,
+        true,
+        true,
+        true,
+        true,
+        false);
+    /*
+    NORSK(
+        "no",
+        "no",
+        "no",
+        "NB",
+        "NB",
+        8,
+        6,
+        false,
+        false,
+        false,
+        false,
+        true)
+    */
+    // TODO: add any new languages here
 
     private final String langCode;
     private final String deeplLangCodeSource;
@@ -32,10 +76,18 @@ public enum LangCodeSelectableList
     private final boolean needSpaceBetweenWords;
 
     @Inject
-    LangCodeSelectableList(String langCode, String deeplCodeSrc, String deeplCodeTgt, int charWidth, int chatBoxCharWidth,
-                           boolean needCharImages, boolean swapMenuOptionAndTarget,
-                           boolean needInputOverlay, boolean needInputCandidateOverlay,
-                           boolean needSpaceBetweenWords){
+    LangCodeSelectableList(
+            String langCode,
+            String deeplCodeSrc,
+            String deeplCodeTgt,
+            int charWidth,
+            int chatBoxCharWidth,
+            boolean needCharImages,
+            boolean swapMenuOptionAndTarget,
+            boolean needInputOverlay,
+            boolean needInputCandidateOverlay,
+            boolean needSpaceBetweenWords)
+    {
         this.langCode = langCode;
         this.deeplLangCodeSource = deeplCodeSrc;
         this.deeplLangCodeTarget = deeplCodeTgt;
@@ -48,23 +100,28 @@ public enum LangCodeSelectableList
         this.needSpaceBetweenWords = needSpaceBetweenWords;
     }
 
-    public boolean needsCharImages() {
+    public boolean needsCharImages()
+    {
         return needCharImages;
     }
-    public boolean needsSwapMenuOptionAndTarget() {
+
+    public boolean needsSwapMenuOptionAndTarget()
+    {
         return swapMenuOptionAndTarget;
     }
 
-    public boolean needsInputOverlay() {
+    public boolean needsInputOverlay()
+    {
         return needInputOverlay;
     }
 
-    public boolean needsInputCandidateOverlay() {
+    public boolean needsInputCandidateOverlay()
+    {
         return needInputCandidateOverlay;
     }
 
-    public boolean needsSpaceBetweenWords() {
+    public boolean needsSpaceBetweenWords()
+    {
         return needSpaceBetweenWords;
     }
-
 }
