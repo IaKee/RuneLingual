9750ddb4076c4d9f7a4cbf71dff821cdbb159ce6
IaKee <giordano.s.mail@gmail.com>
2025-01-23 15:48:34 -0300

    Changes by Guga

--- src/main/java/com/RuneLingual/filemanagement/FileNameAndPath.java
+++ src/main/java/com/RuneLingual/filemanagement/FileNameAndPath.java
@@ -8,7 +8,11 @@ import java.io.File;
 
 public class FileNameAndPath {
     @Getter
-    private static final File localBaseFolder = new File(RuneLite.RUNELITE_DIR.getPath() + File.separator + "RuneLingual_resources");
+    private static final File localBaseFolder = new File(
+        RuneLite.RUNELITE_DIR.getPath()
+            + File.separator
+            + "RuneLingual_resources");
+
     @Getter
     private static final String localSQLFileName = "transcript";
     @Getter @Setter
